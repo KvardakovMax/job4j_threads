c4e805639f1154ede6c4a6159445ba8ce1a2feff
Kvardakov <jimbomaxkvr@gmail.com>
2023-01-25 17:24:49 +0300

    Concurrent output. Testing how 3 threads work

--- src/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/ru/job4j/concurrent/ConcurrentOutput.java
@@ -1,4 +1,20 @@
 package ru.job4j.concurrent;
 
 public class ConcurrentOutput {
+
+    public static void main(String[] args) {
+        Thread firstThread = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        Thread secondThread = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        firstThread.start();
+        secondThread.start();
+
+        System.out.println(Thread.currentThread().getName());
+    }
+
 }
