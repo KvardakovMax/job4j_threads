300144fa1e7c9661deb00f95e7a7228f700e2414
Kvardakov <jimbomaxkvr@gmail.com>
2023-01-25 17:54:09 +0300

    ThreadState. Threads states

---
+++ src/ru/job4j/concurrent/number3threads/ThreadState.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent.number3threads;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        Thread firstThread = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        firstThread.start();
+
+        Thread secondThread = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        secondThread.start();
+
+        while (firstThread.getState() != Thread.State.TERMINATED && secondThread.getState() != Thread.State.TERMINATED) {
+            System.out.println("waiting");
+        }
+        System.out.println(Thread.currentThread().getName() + " - " + firstThread.getState());
+    }
+
+}
