201e4dd87f296e400969a00fcb96f39761378276
Kvardakov <jimbomaxkvr@gmail.com>
2023-01-28 05:25:31 +0300

    interrupt example

---
+++ src/main/java/ru/job4j/concurrent/number5threads/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent.number5threads;
+
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/'};
+        int counter = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(500);
+                if (counter == process.length) {
+                    counter = 0;
+                }
+                System.out.print("\r Load " + process[counter++]);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Unexpected interrupt", e);
+            }
+        }
+    }
+}
