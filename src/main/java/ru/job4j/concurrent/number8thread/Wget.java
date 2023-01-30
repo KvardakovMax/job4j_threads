465c35cb31969469406e50da70bc839be63791d7
Kvardakov <jimbomaxkvr@gmail.com>
2023-01-30 16:40:01 +0300

    wget with speed limiter

---
+++ src/main/java/ru/job4j/concurrent/number8thread/Wget.java
@@ -0,0 +1,50 @@
+package ru.job4j.concurrent.number8thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int waitCountSecond = dataBuffer.length / speed;
+            if (waitCountSecond != 1) {
+                Thread.sleep(1000 * waitCountSecond);
+            }
+
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args[0] == null || Integer.parseInt(args[1]) <= 0) {
+            throw new IllegalArgumentException("Illegal argument!");
+        }
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+
+}
