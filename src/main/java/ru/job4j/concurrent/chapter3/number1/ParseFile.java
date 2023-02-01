69fe41db26b2aa4d7d161df0d64b25edda8c1a5a
Kvardakov <jimbomaxkvr@gmail.com>
2023-02-01 22:58:17 +0300

    solution1. Refactor to strategy

---
+++ src/main/java/ru/job4j/concurrent/chapter3/number1/ParseFile.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent.chapter3.number1;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public class ParseFile {
+
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public String getContent(Predicate<Character> filter) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))) {
+            int data;
+            while ((data = in.read()) != -1) {
+                if (filter.test((char) data)) {
+                    stringBuilder.append((char) data);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+}
